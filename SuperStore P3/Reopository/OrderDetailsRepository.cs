8ba7005d61fcac55e22987a64213e7891336693b
Pk Dagada <36564567@mynwu.ac.za>
2023-09-20 20:01:38 +0200

    Connection String

--- SuperStore P3/Reopository/OrderDetailsRepository.cs
+++ SuperStore P3/Reopository/OrderDetailsRepository.cs
@@ -9,16 +9,57 @@ namespace EcoPower_Logistics.Reopository
     {
         protected readonly SuperStoreContext _context = new SuperStoreContext();
 
-        // GET ALL: Customers
-        public IEnumerable<Customer> GetAll()
+        // GET ALL: OrderDetails
+        public IEnumerable<OrderDetail> GetAll()
         {
-            return _context.Customers.ToList();
+            return _context.OrderDetails.ToList();
         }
 
-        // TO DO: Add ‘Get By Id’
-        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Get By Id’ for OrderDetails
+        public OrderDetail GetById(int orderDetailsId)
+        {
+            return _context.OrderDetails.Find(orderDetailsId);
+        }
+
+        // TO DO: Add ‘Create’ for OrderDetails
+        public void Create(OrderDetail orderDetail)
+        {
+            if (orderDetail == null)
+            {
+                throw new ArgumentNullException(nameof(orderDetail));
+            }
+
+            _context.OrderDetails.Add(orderDetail);
+            _context.SaveChanges();
+        }
+
         // TO DO: Add ‘Edit’
-        // TO DO: Add ‘Delete’
-        // TO DO: Add ‘Exists’
+        public void Edit(OrderDetail orderDetail)
+        {
+            if (orderDetail == null)
+            {
+                throw new ArgumentNullException(nameof(orderDetail));
+            }
+
+            _context.Entry(orderDetail).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        // TO DO: Add ‘Delete’ for OrderDetails
+        public void Delete(int orderDetailsId)
+        {
+            var orderDetail = _context.OrderDetails.Find(orderDetailsId);
+            if (orderDetail != null)
+            {
+                _context.OrderDetails.Remove(orderDetail);
+                _context.SaveChanges();
+            }
+        }
+
+        // TO DO: Add ‘Exists’ for OrderDetails
+        public bool Exists(int orderDetailsId)
+        {
+            return _context.OrderDetails.Any(od => od.OrderDetailsId == orderDetailsId);
+        }
     }
 }
--- SuperStore P3/Reopository/OrdersRepository.cs
+++ SuperStore P3/Reopository/OrdersRepository.cs
@@ -17,10 +17,51 @@ namespace EcoPower_Logistics.Reopository
             return _context.Orders.ToList();
         }
 
-        // TO DO: Add ‘Get By Id’
-        // TO DO: Add ‘Create’
-        // TO DO: Add ‘Edit’
-        // TO DO: Add ‘Delete’
-        // TO DO: Add ‘Exists’
+        // TO DO: Add ‘Get By Id’ for Orders
+        public Order GetById(int orderId)
+        {
+            return _context.Orders.Find(orderId);
+        }
+
+        // TO DO: Add ‘Create’ for Orders
+        public void Create(Order order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException(nameof(order));
+            }
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+
+        // TO DO: Add ‘Edit’ for Orders
+        public void Edit(Order order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException(nameof(order));
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        // TO DO: Add ‘Delete’ for Orders
+        public void Delete(int orderId)
+        {
+            var order = _context.Orders.Find(orderId);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                _context.SaveChanges();
+            }
+        }
+
+        // TO DO: Add ‘Exists’ for Orders
+        public bool Exists(int orderId)
+        {
+            return _context.Orders.Any(o => o.OrderId == orderId);
+        }
     }
 }
